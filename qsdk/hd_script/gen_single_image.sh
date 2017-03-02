94fa36ec433b44865732b4945c233676509ae115
yuzhigang@evergrande.cn <yuzhigang@evergrande.cn>
2017-03-02 11:28:23 +0800

    add default nand-flash.conf for single image.

--- qsdk/hd_script/gen_single_image.sh
+++ qsdk/hd_script/gen_single_image.sh
@@ -11,7 +11,7 @@ cp -rf $QSDK_PATH/bin/ipq806x/openwrt* $QCS_SPF_PATH/IPQ4019.ILQ.4.0/common/buil
 cp -rf $QCS_SPF_PATH/TZ.BF.2.7/trustzone_images/build/ms/bin/MAZAANAA/* $QCS_SPF_PATH/IPQ4019.ILQ.4.0/common/build/ipq
 cp -rf $QCS_SPF_PATH/BOOT.BF.3.1.1/boot_images/build/ms/bin/40xx/misc/tools/config/boardconfig_premium  $QCS_SPF_PATH/IPQ4019.ILQ.4.0/common/build/ipq 
 cp -rf $QCS_SPF_PATH/BOOT.BF.3.1.1/boot_images/build/ms/bin/40xx/misc/tools/config/appsboardconfig_premium  $QCS_SPF_PATH/IPQ4019.ILQ.4.0/common/build/ipq
-
+cp ./nand-flash.conf  $QCS_SPF_PATH/IPQ4019.ILQ.4.0/common/build/ipq/nand-flash.conf
 
 sed -i 's#</linux_root_path>#/</linux_root_path>#' $QCS_SPF_PATH/IPQ4019.ILQ.4.0/contents.xml 
 sed -i 's#</windows_root_path>#\\</windows_root_path>#' $QCS_SPF_PATH/IPQ4019.ILQ.4.0/contents.xml
